f7ac359a0e0425f30191d1aa2cf03f292b907c90
Kuiming <benjamin0901@gmail.com>
2017-03-19 14:21:41 +0800

    while loop for routine renew
    1. auto-marked for overbought and oversold buyer
    2. filter stock according to volume of overbought and oversold

--- main_daily.R
+++ main_daily.R
@@ -28,6 +28,12 @@ main_everyday <- function(recent,date){
   stock <- recent$code
   newurl <- recent$url
   mark <- gs_url(newurl) %>% 
+    gs_read(ws=2) %>% 
+    filter(!is.na(marked))
+  if (dim(mark)[1]>0){
+    return()
+  } 
+  mark <- gs_url(newurl) %>% 
     gs_read(ws=1) %>% 
     filter(!is.na(marked))
   if (dim(mark)[1]==0){
@@ -161,12 +167,12 @@ if (dim(close_stock)[1]>0){
   }
   close_stock$done <- 'done'
   daily[!is.na(daily$close),] <- close_stock
-  wanted <- gs_read(List,ws = 1)
-  wanted$close[wanted$code %in% close_stock$code] <- "done"
-  recent <- gs_read(List,ws = 2)
-  recent$close[recent$code %in% close_stock$code] <- "done"
-  List <- gs_edit_cells(List,ws = 1, input = wanted)
-  List <- gs_edit_cells(List,ws = 2, input = recent)
+  # wanted <- gs_read(List,ws = 1)
+  # wanted$close[wanted$code %in% close_stock$code] <- "done"
+  # recent <- gs_read(List,ws = 2)
+  # recent$close[recent$code %in% close_stock$code] <- "done"
+  # List <- gs_edit_cells(List,ws = 1, input = wanted)
+  # List <- gs_edit_cells(List,ws = 2, input = recent)
   List <- gs_edit_cells(List,ws = 3, input = daily)
 }
 
@@ -183,24 +189,25 @@ if (!is.na(daily$code) && !is.null(date)){
 
 
 stock=gs_read(List,ws=2)
-stock <- filter(stock,is.na(close))
-newstock <- setdiff(stock$code, daily$code)
-recent <- gs_read(List, ws = 2)
-recent <- filter(recent, code %in% newstock)
+recent <- filter(stock,is.na(close))
+
 date=long_date()
 if (dim(recent)[1]>0){
   for (i in 1:dim(recent)[1]){
     main_everyday(recent[i,],date)
   }
   old <- gs_read(List,ws=3)
-  ind <- which(old$code %in% recent$code)
+  ind <- which(old$code %in% recent$code & is.na(old$done))
   write.csv(old[ind,],file = 'newstock.csv',fileEncoding = 'utf8',row.names = F)
   system('/Users/benjamin/anaconda/bin/python /Users/benjamin/Github/stock_conspiracy/move_files.py daily')
   
   new_url <- read.csv('newstock.csv',stringsAsFactors = F,fileEncoding = 'utf8')
-  ind <- which(old$code %in% new_url$code)
+  ind <- which(old$code %in% new_url$code & is.na(old$done))
   old$url[ind] <- new_url$url
   List <- gs_edit_cells(List,ws = 3, input = old)
+  recent <- gs_read(List, ws = 2)
+  recent$close <- "done"
+  List <- gs_edit_cells(List,ws = 2, input = recent)
 }
 
 
